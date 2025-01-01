4ec4051421e53be78176e439fcc24cea7b057b8f
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2025-01-01 12:01:34 -0600

    Non- Blocking asynchronous way

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ const textin = fs.readFileSync("./txt/input.txt", "utf-8");
 const textOut = `This is What we Know about avocado ${textin}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File Written!");
+
+//Non- Blocking, asynchronous way
