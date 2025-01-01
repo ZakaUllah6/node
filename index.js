a3bb70ee3328529e22ea71b5f7d23d6ae3a0cd42
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2024-12-31 23:52:46 -0600

    create and read the file

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const fs = require("fs");
 const textin = fs.readFileSync("./txt/input.txt", "utf-8");
 
 const textOut = `This is What we Know about avocado ${textin}`;
-console.log(textin);
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File Written!");
