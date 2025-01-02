a3eca595d932df28ee2d10952c541090f2a806d2
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2025-01-01 21:09:12 -0600

    Non - Blocking, Asynchronous way

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
 //Blocking, synchronous way
-const textin = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textin = fs.readFileSync("./txt/input.txt", "utf-8");
 
-const textOut = `This is What we Know about avocado ${textin}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written!");
+// const textOut = `This is What we Know about avocado ${textin}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written!");
 
 //Non- Blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("Read the file");
