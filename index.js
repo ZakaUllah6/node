d1fb51fbe6241d581bb450a6ce673e2da750166d
Zaka Ullah <44844487+ZakaUllahMomannD@users.noreply.github.com>
2025-01-04 18:11:00 -0600

    Creatin an API

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/api") {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       const productData = JSON.parse(data);
-      console.log(productData);
-      res.writeHead(200, { "Content-type ": "application/json" });
+
+      res.writeHead(200, { "content-type": "application/json" });
       res.end(data);
     });
   } else {
